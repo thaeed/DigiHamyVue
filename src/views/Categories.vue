c6eb5e0d4b520d3fde44ce2f3ef0ec45cc852121
thaeed <mr_saeed_da@yahoo.com>
2022-01-30 19:54:31 +0330

    hmm wtf at_work

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,13 +1,42 @@
 <template>
-  {{id}}
+  <PageComponent>
+    {{ cats }}
+  </PageComponent>
 </template>
 
 <script setup>
 import { useRoute } from "vue-router";
+import { computed } from "vue";
+import axios from "axios";
 
-const route = useRoute();  
-const id = route.params.id; // read parameter id (it is reactive) 
+const route = useRoute();
+const id = route.params.id; // read parameter id (it is reactive)
+const cats = computed(() => apiCall);
 
+async function apiCall() {
+  try {
+    let res = axios({
+      url: "https://localhost:5001/course",
+      method: "get",
+      timeout: 8000,
+      headers: {
+        ApiKey: "ouur12%^074$!^0QWYE#!WU78rt12bU!gg&",
+      },
+    });
+    if (res.status == 200) {
+      // test for status you want, etc
+      console.log(res.status);
+      console.log(res.data);
+      console.log("####");
+    }
+    // Don't forget to return something
+    return (await res).data;
+  } catch (err) {
+    console.error(err);
+  }
+}
 </script>
+
+<script mounted></script>
 
 <style scoped></style>
